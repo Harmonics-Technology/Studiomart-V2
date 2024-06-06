375f4385eeff07fdc698b1667b8cf5a08a26bf7c
adelowomi <adelowomi@gmail.com>
2024-06-06 15:33:36 +0100

    Remove ignored files

--- src/lib/components/Messaging/Input.tsx
+++ src/lib/components/Messaging/Input.tsx
@@ -10,8 +10,8 @@ import {
 } from '@chakra-ui/react';
 import emojiData from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 // @ts-expect-error - This is a custom function
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { uuidv4 } from '@firebase/util';
 import { Widget } from '@uploadcare/react-widget';
 import { arrayUnion, doc, Timestamp, updateDoc } from 'firebase/firestore';
